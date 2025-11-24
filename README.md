858954bf19d079ef81e0b9c4910996e7252b6f53
unknown <yuvrajg2442@gmail.com>
2025-11-25 00:22:59 +0530

    Initial commit: add README and calc.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# Intentional Merge Conflict Demo
+A tiny Python tool to demonstrate merge conflict creation & resolution.
---
+++ calc.py
@@ -0,0 +1,13 @@
+# calc.py - simple calculator
+VERSION = "v1.0"
+
+def add(a, b):
+    return a + b
+
+def main():
+    # Greeting line that we'll intentionally change on branches
+    print("Hello from main branch - version " + VERSION)
+    print("2 + 3 =", add(2, 3))
+
+if __name__ == "__main__":
+    main()
