1333e68cecdb9143ef655b309eab141a499230c9
unknown <yuvrajg2442@gmail.com>
2025-11-25 00:28:15 +0530

    feature-A: change greeting to feature-A

--- calc.py
+++ calc.py
@@ -6,8 +6,9 @@ def add(a, b):
 
 def main():
     # Greeting line that we'll intentionally change on branches
-    print("Hello from main branch - version " + VERSION)
+    print("Hello from feature-A branch - version " + VERSION)
     print("2 + 3 =", add(2, 3))
 
 if __name__ == "__main__":
     main()
+    
